c4692c0b8a13a54c6ba4fd679cb4b1503de0f6fb
paulMorea <paul_morea@hotmail.fr>
2021-09-19 16:13:26 +0200

    correction jeu de base + intégration des modules aller + loin

--- js/main.js
+++ js/main.js
@@ -1,45 +1,62 @@
 // Message Accueil
-alert("Bonjour ! Bienvenue sur Shi Fu Mi vs Machine");
+alert("Bonjour ! Bienvenue sur Shi Fu Mi: Man vs Machine");
 // Prénom
 var firstname = prompt("Merci de rentrer votre prénom :");
-// Choix joueur
-var choix = prompt(firstname + " merci de choisir entre Pierre, Feuille, Ciseaux (taper votre choix): ");
-var choixMe = choix.toLowerCase();
-console.log(choixMe);
+// Initialise la partie
+var resultat = 1;
+var myScore =0;
+var machineScore =0;
+// Choix joueur    
+while (machineScore<3 && resultat ===1|| myScore<3 && resultat ===1){  
+var choixMe = prompt(firstname + " merci de choisir entre Pierre, Feuille, Ciseaux (taper votre choix): ");
+var choixMe = choixMe.toLowerCase();
+
 // Choix machine
-var machine = Math.floor(Math.random() * (4 - 1)) + 1;
-if(machine === 1){
-    machine="pierre";
-    alert("La Machine a choisit Pierre");
-}
-else if(machine === 2) {
-    machine="feuille";
-    alert("La Machine a choisit Feuille");
-}
-else{
-    machine="ciseaux"
-    alert("La Machine a choisit Ciseaux");
-}
+    var machine = Math.floor(Math.random() * (4 - 1)) + 1;
+    if(machine === 1){
+        machine="pierre";
+        alert("La Machine a choisit Pierre");
+    }
+    else if(machine === 2) {
+        machine="feuille";
+        alert("La Machine a choisit Feuille");
+    }
+    else{
+        machine="ciseaux"
+        alert("La Machine a choisit Ciseaux");
+    }
+
+//  Résultat partie
+    if (choixMe===machine){    
+        alert("Égalité !");  
+    }
+    else if (choixMe==="pierre" && machine==="feuille" || choixMe==="feuille" && machine==="ciseaux" || choixMe==="ciseaux" && machine==="pierre"){
+        machineScore += 1;
+        alert("La Machine gagne cette manche! Moi : " + myScore + " / Machine : " + machineScore);
+        
+    }
+    else {
+        myScore += 1;
+        alert("Tu gagnes cette manche! Moi : " + myScore + " / Machine : " + machineScore);
+        
+    }
 //  Résultat match
-if (choixMe===machine){
-    prompt("Égalité !");
-    choix;
-}
-else if (choixMe==="pierre" && machine==="feuille"){
-    alert("Machine win");
-}
-else if (choixMe==="pierre" && machine==="ciseaux"){
-    alert("You win");
-}
-else if (choixMe==="feuille" && machine==="ciseaux"){
-    alert("Machine win");
-}
-else if (choixMe==="feuille" && machine==="pierre"){
-    alert("You win");
-}
-else if (choixMe==="ciseaux" && machine==="pierre"){
-    alert("Machine win");
-}
-else {
-    alert("You win");
+    if (myScore === 3 || machineScore ===3){
+        if( myScore === 3){
+            alert("U WIN"); 
+        }
+        else if(machineScore ===3){
+            alert("MACHINE WIN");
+        }
+        var rejouer = prompt("Voulez-vous rejouez? o/n")
+        if(rejouer === "n"){
+            resultat = 0;
+            alert("A bientot"); 
+        }
+        else{
+            var myScore =0;
+            var machineScore =0;
+            resultat= 1;
+        }
+    }
 }
